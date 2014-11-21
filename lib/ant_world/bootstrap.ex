72c19051d9bccf026518b4f8a09d050dd414bf9a
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-11-21 08:22:24 -0500

    one loop is enough

--- lib/ant_world/bootstrap.ex
+++ lib/ant_world/bootstrap.ex
@@ -3,11 +3,12 @@ defmodule AntWorld.Bootstrap do
   def world_of_ants(width, height, nb_food, nb_ants) do
     foods = FoodGenerator.random_food_position(width, height, nb_food)
     world_pid = AntWorld.World.start_link(width, height, foods)
-    # ant_pid = AntWorld.Ant.start_link( world_pid )
-    # send ant_pid, :grass
-    # {world_pid, [ant_pid]}
-    ants = Enum.map 1..nb_ants, fn (_) -> AntWorld.Ant.start_link( world_pid ) end
-    Enum.each ants, fn (ant_pid) -> send ant_pid, :grass end
+
+    ants = Enum.map 1..nb_ants, fn (_) ->
+      ant_pid = AntWorld.Ant.start_link( world_pid )
+      send ant_pid, :grass
+      ant_pid
+    end
     {world_pid, ants}
   end
 end
